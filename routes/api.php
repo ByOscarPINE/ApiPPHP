8a32c9f40370760b09969501dd3a596571cad2aa
byOscarPINE <emanuelsant768@gmail.com>
2025-02-09 09:47:49 -0600

    2

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,7 @@ use App\Http\Middleware\Authenticate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
